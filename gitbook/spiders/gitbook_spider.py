d1c2c85ff4bd9185fa07bf70254455cb73bbcc32
trumanliu <yangyangouc@gmail.com>
2015-04-26 16:17:50 +0800

    将首页书籍的抓取放入到方法中，并在首页读取页面范围

--- gitbook/spiders/gitbook_spider.py
+++ gitbook/spiders/gitbook_spider.py
@@ -8,6 +8,7 @@ class GitBookSpider(scrapy.Spider):
 	name = "gitbook"
 	allowed_domains = ["gitbook.com"]
 	start_urls = ["https://www.gitbook.com/explore/"]
+	#解析页面内容并组成对象
 	def parse_item(self,response):
 		sel = Selector(response)
 		books=[]
@@ -15,33 +16,22 @@ class GitBookSpider(scrapy.Spider):
 		for index, link in enumerate(bookdivs):
 			book = GitbookItem();
 			book['rbookName']=link.xpath('.//a[@class="book-cover"]/@href').extract()
-			book['rabookName']=link.xpath('.//div[@class="mask-inner"]/h3').extract()
-			book['readCount']=link.xpath('.//div[@class="book-infos"]/p').extract()
+			book['rabookName']=link.xpath('.//div[@class="mask-inner"]/h3//text()').extract()
+			book['readCount']=link.xpath('.//div[@class="book-infos"]/p//text()').extract()
 			book['coverAddress']=link.xpath('.//img/@src').extract()
 			books.append(book)
 		return books
 
-
-
 	def parse(self, response):
-		bookdivs = response.xpath('//div[@class="book col-xs-6 col-sm-3 col-md-3 col-lg-3"]')
-		books = [];
+		#获取最后一页页码
+		lastNum = response.xpath('//ul[@class="pagination pagination-pages"]//li[5]//text()').extract()
 		validurls = []
-		#设置递归访问页面,页面范围稍后改为从起始页面读取
-		for i in range(1, 234):
+		#添加首页
+		validurls.append("https://www.gitbook.com/explore")
+		#设置递归访问页面
+		for i in range(1, int(lastNum[0])):
 			validurls.append('https://www.gitbook.com/explore?page='+str(i))
-		for index, link in enumerate(bookdivs):
-			book = GitbookItem();
-			book['rbookName']=link.xpath('.//a[@class="book-cover"]/@href').extract()
-			book['rabookName']=link.xpath('.//div[@class="mask-inner"]/h3').extract()
-			book['readCount']=link.xpath('.//div[@class="book-infos"]/p').extract()
-			book['coverAddress']=link.xpath('.//img/@src').extract()
-			yield book
 		for url in validurls:
+			#依次抓取内容
 			yield Request(url, callback=self.parse_item)
 
-
-#print link.xpath('.//a[@class="book-infos"]/p').extract()
-#link = sel.xpath('a/@href').extract()
-#desc = sel.xpath('text()').extract()
-#print title, link, desc
